9ea7417e69981207183e0fd7a8caac3da68b8939
Tejan <tchin018@uottawa.ca>
2016-10-29 03:01:55 +0000

    Added tone.c which will generate a wav file with 44.1 Khz sampling rate and a 880 Hz tone. Modified app.c to enable compilation of my code. wav.h is the header file used by audio_recorder.c Audio_recorder.c is supposed to record audio but has not been tested yet.

--- bbb/meta-bbb/recipes-core/trebletrouble/files/src/app.c
+++ bbb/meta-bbb/recipes-core/trebletrouble/files/src/app.c
@@ -8,6 +8,8 @@
 
 #include <stdio.h>
 #include "metronome.h"
+#include "tone.h"
+#include "audio_recorder.h"
 
 int main(int argc, char** argv) {
 
@@ -43,8 +45,13 @@ int main(int argc, char** argv) {
 		compare_notes(expected[i], actual[i], i, fbp);
 		sleep(1);
 	}
+
 	cleanup_display(fbp, &fbfd);
 	while(1); /* fuck it */
 	metronome();
+
+	// Added tone.c which will generate a wav file with 44.1 Khz sampling rate and a 880 Hz tone.
+	tone();
+
 	return 0;
 }
