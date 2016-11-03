6dfd93c5689c57c0821fa109cf416349a1bfc9a1
Tejan <tchin018@uottawa.ca>
2016-11-03 22:23:54 +0000

    Topic: Working code for Demo
    This commit has working code for our demo.

--- bbb/meta-bbb/recipes-core/trebletrouble/files/src/app.c
+++ bbb/meta-bbb/recipes-core/trebletrouble/files/src/app.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
-
 #include "display.h"
-#include <stdio.h>
 #include "metronome.h"
 #include "tone.h"
 #include "audio_recorder.h"
 
 int main(int argc, char** argv) {
 
-	char* fbp;
-	int fbfd, err, temp;
-
+        char* fbp;
+  	int fbfd, err, temp;
+/*
 	fbp = init_display(&fbfd);
 
 	err = bitblit("/srv/trebletrouble/timbit.pnm", fbp, 400, 240);
@@ -36,9 +34,12 @@ int main(int argc, char** argv) {
 	sleep(10);
 
 	cleanup_display(fbp, &fbfd);
+<<<<<<< HEAD
 	metronome();
 	return 0; 
 
+=======
+  */
 	// Added tone.c which will generate a wav file with 44.1 Khz sampling rate and a 880 Hz tone.
 	temp = tone();
 
