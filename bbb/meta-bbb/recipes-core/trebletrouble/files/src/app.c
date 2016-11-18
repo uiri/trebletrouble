bd977f514055b913b2abf353613ea35af523ae1f
Will <uiri00@gmail.com>
2016-11-17 21:47:33 -0500

    Add input code
    Fix colours.
    Add input to Makefile.
    Have app.c do some testing for this part of the code. This may make
        integrating annoying later

--- bbb/meta-bbb/recipes-core/trebletrouble/files/src/app.c
+++ bbb/meta-bbb/recipes-core/trebletrouble/files/src/app.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#include "display.h"
 #include "colours.h"
+#include "display.h"
+#include "input.h"
 
 #define NUM_NOTES 16
 
 int main(int argc, char** argv) {
 	char* fbp;
-	int fbfd, err, i;
+	int fbfd, tsfd, err, i;
 	int actual[NUM_NOTES] = {39, 41, 43, 44, 46, 48, 49, 51, 53, 55, 56, 58, 60, 62, 63, 65};
 	int expected[NUM_NOTES] = {39, 41, 43, 44, 46, 48, 50, 51, 53, 55, 56, 58, 60, 62, 63, 65};
+	ScreenBounds sb;
 
 	fbp = init_display(&fbfd);
+
+	err = init_touchscreen(&tsfd, &sb);
+	if (err) {
+		printf("Error reading event input file\n");
+		return err;
+	}
+
 	colour_screen(fbp, ORANGE);
 	err = bitblit("/srv/trebletrouble/timbit.pnm", fbp, 400, 240);
-
 	if (err) {
 		if (err == -1) {
 			printf("File not found :(");
@@ -39,6 +47,20 @@ int main(int argc, char** argv) {
 		compare_notes(expected[i], actual[i], i, fbp);
 		sleep(1);
 	}
+
+	while (1) {
+		si = get_lcd_input(&tsfd, &sb);
+		/* Need to figure out what to do with the input once we 
+		   have it */
+		/* if (si.y < 512) */
+		/* 	colour_screen(fbp, RED); */
+		/* else if (si.y < 3930) */
+		/* 	colour_screen(fbp, GREEN); */
+		/* else */
+		/* 	colour_screen(fbp, ORANGE); */
+		sleep(1);
+	}
+
 	cleanup_display(fbp, &fbfd);
 	while(1); /* fuck it */
 	return 0;
