3576106e5ef5fed0a68d21fdc6378f21058d9d64
Tejan <tchin018@uottawa.ca>
2016-11-25 03:30:25 +0000

    Pitch recognition finally works!!!!!!!!!!

--- bbb/meta-bbb/recipes-core/trebletrouble/files/src/app.c
+++ bbb/meta-bbb/recipes-core/trebletrouble/files/src/app.c
@@ -19,6 +19,8 @@ int main(int argc, char** argv) {
 	}
 
 	int expected[NUM_NOTES] = {39, 41, 43, 44, 46, 48, 50, 51, 53, 55, 56, 58, 60, 62, 63, 65};
+	int freq;
+	float pitch;
 
 	colour_screen(fbp, ORANGE);
 	get_lcd_input(&si);
@@ -34,3 +36,5 @@ int main(int argc, char** argv) {
 	cleanup_display(fbp, &fbfd);
 	return 0;
 }
+
+
