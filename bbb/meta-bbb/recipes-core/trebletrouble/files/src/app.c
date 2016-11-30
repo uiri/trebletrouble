43d4412d67a1927f84d11441659305e15952218d
Tejan <tchin018@uottawa.ca>
2016-11-30 22:07:56 +0000

    1) I separated my test code for jingle-bells into a new file tone_test.c 2) Modified tone.c with Will's PR requests.

--- bbb/meta-bbb/recipes-core/trebletrouble/files/src/app.c
+++ bbb/meta-bbb/recipes-core/trebletrouble/files/src/app.c
@@ -8,10 +8,7 @@
 #include "display.h"
 #include "input.h"
 
-#include <stdio.h>
 #include "metronome.h"
-#include "tone.h"
-#include "libfft.h"
 #include "audio_recorder.h"
 
 int main(int argc, char** argv) {
@@ -33,13 +30,6 @@ int main(int argc, char** argv) {
 	}
 
 	int expected[NUM_NOTES] = {39, 41, 43, 44, 46, 48, 50, 51, 53, 55, 56, 58, 60, 62, 63, 65};
-	int freq;
-
-	int numFrequencies = 11;
-	int freqList[numFrequencies]; 
-	float durationList[numFrequencies];
-	float pitch[numFrequencies];
-	float duration;
 
 	colour_screen(fbp, ORANGE);
 	err = bitblit("/srv/trebletrouble/timbit.pnm", fbp, 400, 240);
@@ -84,42 +74,6 @@ int main(int argc, char** argv) {
 	while(1); /* fuck it */
 	cleanup_display(fbp, &fd.fb);
 	metronome();
-
-	/*JINGLE BELLS*/
-	freqList[0]  = 330;
-	freqList[1]  = 330;
-	freqList[2]  = 330;
-	freqList[3]  = 330;
-	freqList[4]  = 330;
-	freqList[5]  = 330;
-	freqList[6]  = 330;
-	freqList[7]  = 391;
-	freqList[8]  = 261;
-	freqList[9]  = 293;
-	freqList[10] = 330;
-
-	durationList[0]  = 1.0;  /* E  */
-	durationList[1]  = 1.0;  /* E  */
-	durationList[2]  = 2.0;  /* E  */
-  
-	durationList[3]  = 1.0;  /* E  */
-	durationList[4]  = 1.0;  /* E  */
-	durationList[5]  = 2.0;  /* E  */
- 
-	durationList[6]  = 1.0;  /* E  */
-	durationList[7]  = 1.0;  /* G  */
-	durationList[8]  = 1.0;  /* C  */
-	durationList[9]  = 1.0;  /* D  */
-	durationList[10] = 2.0;  /* E  */
-
-	/* generate wave data */ 
-	for (i = 0; i < numFrequencies; i++) {
-	  freq = freqList[i];
-	  duration = durationList[i];
-	  pitch[i] = get_pitch(freq,duration);
-	  printf("%f\n",pitch[i]);
-	  sleep(duration);
-	}
   
 	return 0;
 
