1fac1f0e5a35a50ab254a8a2296e06ad053cdb9a
Tejan <tchin018@uottawa.ca>
2016-12-01 18:24:55 +0000

    I deleted tone.o and wav.h using git rm

--- bbb/meta-bbb/recipes-core/trebletrouble/files/src/wav.h
+++
@@ -1,6 +0,0 @@
-#include <stdint.h>
-
-WaveHeader *genericWAVHeader(uint16_t bit_depth, uint16_t channels);
-WaveHeader *retrieveWAVHeader(const void *ptr);
-int writeWaveHeader(int fd, WaveHeader *hdr);
-int recordWAV(const char *fileName, WaveHeader *hdr, uint32_t duration);
