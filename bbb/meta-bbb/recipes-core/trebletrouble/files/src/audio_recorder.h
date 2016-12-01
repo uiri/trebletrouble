e62a5df3ffa51c0f73a2a28fe32f4b47b06587fc
Tejan <tchin018@uottawa.ca>
2016-12-01 18:43:35 +0000

    Modified the goto labels for PCM and BUF.

--- bbb/meta-bbb/recipes-core/trebletrouble/files/src/audio_recorder.h
+++ bbb/meta-bbb/recipes-core/trebletrouble/files/src/audio_recorder.h
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include "tone.h"
 
+void audio_recorder(void);
 WaveHeader *genericWAVHeader(uint16_t bit_depth, uint16_t channels);
 WaveHeader *retrieveWAVHeader(const void *ptr);
 int writeWaveHeader(int fd, WaveHeader *hdr);
