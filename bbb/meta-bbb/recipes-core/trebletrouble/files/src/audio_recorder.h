95943823b05442617b31c4ae36307f344143ca76
Tejan <tchin018@uottawa.ca>
2017-02-19 22:29:01 +0000

    Add audio recorder

---
+++ bbb/meta-bbb/recipes-core/trebletrouble/files/src/audio_recorder.h
@@ -0,0 +1,10 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include "tone.h"
+
+void audio_recorder(Wave* wave, uint32_t duration);
+void genericWAVHeader(WaveHeader *hdr, uint16_t bit_depth, uint16_t channels);
+/*WaveHeader *retrieveWAVHeader(const void *ptr);*/
+int writeWaveHeader(int fd, WaveHeader *hdr);
+int recordWAV(Wave* wave, WaveHeader* hdr, uint32_t duration);
+
